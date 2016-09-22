b5d937e9cef48e78084b04a405001dfa1f2b8436
mikeadossi <mikeadossi@gmail.com>
2016-09-21 21:08:41 -0700

    we learn about url :variables

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ expressApp.get('/', function(req, res){ // If the user makes a browser request f
 	res.send("This is a server response on the home page");
 });
 
-expressApp.get('/darkKnight', function(req, res){
-	res.send("This is a server response on the DK page");
-})
+//Below we learn how to pass 'variables' to the end of our url to request specific pages from our server and have the info returned changed dynamically. The colon on line 13 signifies a dynamic variable follows!
+expressApp.get('/nolan_movie/:movie_number?', function(req, res){
+	var movie_number = req.params.movie_number;
+	res.send("This is the page for nolan movie " + movie_number);
+});
 
 expressApp.listen(3000, function(){
 	console.log("The application is running on localhost3000");
-})
+});
