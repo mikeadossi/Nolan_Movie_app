457bdad9f83b56ecc7787545ecd61725a6cf155f
mikeadossi <mikeadossi@gmail.com>
2016-09-21 21:43:06 -0700

    we render a home.ejs page upon index req

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 
 var expressApp = express(); //-> 'app' is the typically what's used here
 
+expressApp.set('view engine', 'ejs'); //The 1st step in using ejs templating goodness we specify it as our expressApps view engine 
+
+
+
 // Routes 
 
 // Home
 expressApp.get('/', function(req, res){ // If the user makes a browser request for the '/' (index) page the server responds with the following:
-	res.send("This is a server response on the home page");
+	res.render('home', {
+		title : "Nolan Movies"
+	});
 });
 
 //Below we learn how to pass 'variables' to the end of our url to request specific pages from our server and have the info returned changed dynamically. The colon on line 13 signifies a dynamic variable follows!
