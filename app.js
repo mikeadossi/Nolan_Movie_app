234aeb79f6bbfc9223ba07222b4bc1fd6877266f
mikeadossi <mikeadossi@gmail.com>
2016-09-22 00:06:07 -0700

    exported out routing functions and edited app.js

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ var expressApp = express(); //-> 'app' is the typically what's used here
 
 expressApp.set('view engine', 'ejs'); //The 1st step in using ejs templating goodness we specify it as our expressApps view engine 
 
-
+var routes = require('./routes');
 
 // Routes 
 
+// home
+expressApp.get('/', routes.home);
+
+// movie_single
+expressApp.get('/nolan_movies/:movie_number?', routes.movie_single);
+
+// notFound
+expressApp.get('*', routes.not_found);
+
 expressApp.listen(3000, function(){
-	console.log("The application is running on localhost3000");
+	console.log('The application is running on localhost3000');
 });
