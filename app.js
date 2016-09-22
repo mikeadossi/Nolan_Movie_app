7f3b42328d28e30d943ec59f3de52ce1f0f47404
mikeadossi <mikeadossi@gmail.com>
2016-09-21 20:43:48 -0700

    express listen and get functions

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var expressApp = express(); //-> 'app' is the typically what's used here
+
+// Routes 
+
+// Home
+expressApp.get('/', function(req, res){ // If the user makes a browser request for the '/' (index) page the server responds with the following:
+	res.send("This is a server response on the home page");
+});
+
+expressApp.listen(3000, function(){
+	console.log("The application is running on localhost3000");
+})
