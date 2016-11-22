2d47d53365e1690b70968ea438a3c2ab215ca326
mikeadossi <mikeadossi@gmail.com>
2016-11-21 19:28:00 -0800

    changes to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,9 @@ exports.home = function(req, res){ // If the user makes a browser request for th
 
 // movie_single
 exports.movie_single = function(req, res){
-	var episode_number = req.params.episode_number; // this route is activated by movie_single code in our app.js file. Note: this route takes in more than one page.
+	var movie_number = req.params.movie_number; // this route is activated by movie_single code in our app.js file. Note: this route takes in more than one page.
 	var movies = moviesJSON.movies;
-	var movie = movies[episode_number - 1]; // Here we store in our variable a way for our users grab the exact movie they want. Arrays are zero based so we must account for that with the -1.
+	var movie = movies[movie_number - 1]; // Here we store in our variable a way for our users grab the exact movie they want. Arrays are zero based so we must account for that with the -1.
 	var title = movie.title;
 	res.render('movie_single', {
 		movies : movies,
