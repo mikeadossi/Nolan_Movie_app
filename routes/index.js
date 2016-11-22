1293f8552f03cb6f73c6a2fad340b54d71618c42
mikeadossi <mikeadossi@gmail.com>
2016-11-21 19:53:56 -0800

    added 'notFound.ejs' page & rendering function

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,41 @@ var moviesJSON = require('../movies.json');
 // Home
 exports.home = function(req, res){ // If the user makes a browser request for the '/' (index) page the server responds with the following:
 
-	var movies = moviesJSON.movies;
+		var movies = moviesJSON.movies;
 
-	res.render('home', {
-		title : "Nolan Movies",
-		movies: movies
-	});
+		res.render('home', {
+			title: "Nolan Movies",
+			movies : movies
+		});
+	};
+
+	// movie_single
+	exports.movie_single = function(req, res){
+		var movie_number = req.params.movie_number; // this route is activated by movie_single code in our app.js file. Note: this route takes in more than one page.
+		var movies = moviesJSON.movies;
+	// only render the movie_single template for episodes that exist
+	if (movie_number >= 1 && movie_number <= 14){
+		var movie = movies[movie_number - 1];
+		var title = movie.title;
+		var starring_actors = movie.starring_actors;
+		var year_of_release = movie.year_of_release;
+
+		res.render('movie_single', {
+			movie : movie,
+			title : title,
+			movies: movies,
+			movie_number : movie_number,
+			year_of_release : year_of_release,
+			starring_actors : starring_actors
+		});
+	} else {
+			res.render('notFound', {
+					movies : movies,
+					title : "Oops, this page doesn't exist"
+			})
+	}
 };
 
-// movie_single
-exports.movie_single = function(req, res){
-	var movie_number = req.params.movie_number; // this route is activated by movie_single code in our app.js file. Note: this route takes in more than one page.
-	var movies = moviesJSON.movies;
-	var movie = movies[movie_number - 1]; // Here we store in our variable a way for our users grab the exact movie they want. Arrays are zero based so we must account for that with the -1.
-	var title = movie.title;
-	res.render('movie_single', {
-		movies : movies,
-		title : title
-	});
-};
 
 // not_found
 exports.not_found =  function(req, res){
